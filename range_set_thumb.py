0a63da8b2ca3e97fc1beb10ee5aa151334e87cdb
yaomai <yaomai@tencent.com>
2023-01-30 18:18:08 +0800

    add range set thumb

---
+++ range_set_thumb.py
@@ -0,0 +1,16 @@
+import idc
+import os
+import ida_idaapi
+
+if __name__ == "__main__":
+    start = read_selection_start()
+    end = read_selection_end()
+    if start == ida_idaapi.BADADDR:
+        print("start is BADADDR please select a range")
+    addr = start
+    print("set thumb 0x%08x - 0x%08x"%(start, end))
+    while addr != idc.BADADDR and addr < end:
+        #改tf寄存器成1
+        idc.split_sreg_range(addr, "T", 1, SR_user)
+        addr = idc.next_head(addr)
+    #
